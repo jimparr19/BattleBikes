8c35d79bd3bcf32b6bf538814fb0f9a4adc2cf87
David Walker <dwalker0044@gmail.com>
2016-05-22 10:12:28 +0100

    added app directory

---
+++ src/http_flask_server.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+
+# ----------------------------------------------------------------------------
+
+@app.route("/")
+def hello():
+    return render_template('root.html', value=1)
+
+# ----------------------------------------------------------------------------
+
+
+def main(mode, port):
+    app.run()
