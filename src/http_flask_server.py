4d8ee573f0b56b0602be26cb60119fe3e8eafa6a
David Walker <dwalker0044@gmail.com>
2016-05-25 19:20:27 +0100

    Some code to think about how flask interacts with the database.

--- src/http_flask_server.py
+++ src/http_flask_server.py
@@ -2,17 +2,28 @@ from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 
+# from player import Player
 
+# Player is the database accessor. It is meant to hide the calls to mongodb.
+# player_1 = Player("Player1")
+# player_2 = Player("Player2")
 # ----------------------------------------------------------------------------
 
 @app.route("/")
 def hello():
+    # We can then pull data out of the database by using the Player object.
+    # data = player_1.get_speed()
+
     return render_template('root.html', value=1)
 
 # ----------------------------------------------------------------------------
 
 
 def main(mode, port):
+    # start opens up the database - not the best name really.
+    # player_2.start()
+    # player_1.start()
+
     app.run()
 
 
