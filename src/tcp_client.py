8a31c5bdf57703b1b456e27ac55de30d2a78ce35
David Walker <dwalker0044@gmail.com>
2016-05-25 19:22:38 +0100

    Added a period event to sample the sensor.

--- src/tcp_client.py
+++ src/tcp_client.py
@@ -17,16 +17,58 @@ def signal_handler(signal, frame):
     terminate()
 
 
+# ***This is where the sensor sampling code needs to go!***
+def sample_sensor():
+    print("Sampling sensor")
+    return bytes([1])
+
+
+class PeriodicEvent:
+    def __init__(self, function):
+        self.last_exec = 0.0
+        self.period = 1.0
+        self.func = function
+
+    def poll(self, now):
+        diff = (now - self.last_exec)
+
+        if diff >= self.period:
+            self.last_exec = now
+            return self
+
+        return None
+
+    def exec(self):
+        return self.func()
+
+
 def main_loop():
+    # TODO: this requires that the tcp serer is already running otherwise the
+    # connection will fail and the program will exit. We should attempt to 
+    # reconnect at regular intervals.
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect(('127.0.0.1', 1500))
 
+    # PeriodicEvent is really just a way of handling a function that you want
+    # call periodically without cluttering the mainloop. Defaults to 1 second.
+    # You should 'poll' it every loop iteration and it will tell you if its
+    # time to execute the function.
+    sample = PeriodicEvent(sample_sensor)
+
     while True:
-        client_socket.send(b"hello")
-        time.sleep(30)
+        data = None
+        if sample.poll(time.time()):
+            data = sample.exec()
+
+        if data:
+            client_socket.send(data)
+            pass
 
         if TERMINATE:
             break
+
+        # You can slow down the mainloop by introducing a sleep.
+        # time.sleep(0.5)
 
     print("***Terminated TCP Client***")
 
