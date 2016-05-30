ef260507e706169bdf1f9f8283dead0fa894ec43
jimparr19 <jimparr19@gmail.com>
2016-05-30 21:28:43 +0100

    added single player web app. using a random function as a placeholder for now and styling to come

---
+++ src/single_player_app.py
@@ -0,0 +1,26 @@
+from flask import Flask, Response, stream_with_context
+
+import random
+import json
+import time
+
+app = Flask(__name__)
+
+def stream_template(template_name, **context):
+    app.update_template_context(context)
+    t = app.jinja_env.get_template(template_name)
+    rv = t.stream(context)
+    return rv
+
+@app.route('/stream')
+def index():
+    def g():
+        d = [dict(x=0, y=0)]
+        for i in range(1,500):
+            time.sleep(random.random())  # an artificial random delay
+            d.append(dict(x=i, y=random.random()))
+            yield json.dumps(d)
+    return Response(stream_template('single_player.html', data=g()))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=9000, debug=True)
